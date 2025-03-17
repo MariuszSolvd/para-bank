0f2e422c30e39681bc63766548555dad318c3879
MariuszSolvd <mkaminski@solvd.com>
2025-03-17 12:53:51 +0100

    Added WebDriverFactory

---
+++ src/main/java/com/solvd/driver/WebDriverFactory.java
@@ -0,0 +1,20 @@
+package com.solvd.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver createDriver() {
+        String browser = ConfigReader.get("browser");
+
+        return switch (browser.toLowerCase()) {
+            case "chrome" -> new ChromeDriver();
+            case "firefox" -> new FirefoxDriver();
+            case "edge" -> new EdgeDriver();
+            default -> throw new IllegalArgumentException("Incorrect web browser: " + browser);
+        };
+    }
+}
