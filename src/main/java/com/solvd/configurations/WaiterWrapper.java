343486958badb1c6e7f36e971f551029fea9a661
MariuszSolvd <mkaminski@solvd.com>
2025-03-17 15:52:08 +0100

    Add Overloading to WaiterWrapper

--- src/main/java/com/solvd/configurations/WaiterWrapper.java
+++ src/main/java/com/solvd/configurations/WaiterWrapper.java
@@ -10,11 +10,19 @@ import java.time.Duration;
 
 public class WaiterWrapper {
 
+    private static WebDriverWait getWait(WebDriver webDriver) {
+        return new WebDriverWait(webDriver, Duration.ofSeconds(30));
+    }
+
     private static WebDriverWait getWait(WebDriver webDriver, int timeoutSec) {
         return new WebDriverWait(webDriver, Duration.ofSeconds(timeoutSec));
     }
 
     public static WebElement waitForElementVisible(WebDriver driver, int timeoutSec, By locator) {
         return getWait(driver, timeoutSec).until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static WebElement waitForElementVisible(WebDriver webDriver, By locator) {
+        return getWait(webDriver).until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
