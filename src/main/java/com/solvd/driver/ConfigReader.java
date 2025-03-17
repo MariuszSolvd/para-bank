7e845b04e2311f5d90dce24f07bba0d00f38df89
MariuszSolvd <mkaminski@solvd.com>
2025-03-17 12:18:45 +0100

    Set ConfigReader

---
+++ src/main/java/com/solvd/driver/ConfigReader.java
@@ -0,0 +1,24 @@
+package com.solvd.driver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties;
+
+    static {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/test/resources/config.properties");
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException exception) {
+            throw new RuntimeException("Cannot read config file");
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
